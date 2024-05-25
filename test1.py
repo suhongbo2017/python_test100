cadd3d33c3b6fecfeda0208ab41427b8ffc2d11a
suhongbo2017 <suhongbo2017@gmail.com>
2024-05-25 15:33:57 +0800

    测试题1时间太短无法计算运行时间

--- test1.py
+++ test1.py
@@ -3,7 +3,8 @@ import time
 start = time.time()
 lst = [[a,b,c] for a in range(5) for b in range(5) for c in range(5) if (a != b) and (a != c) and (b != c)]
 end = time.time()
-print(start - end)
+
+
 print(lst)
 
 
