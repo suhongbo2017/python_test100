66169c999dec4441527fe77ff875c0a5ea4c5668
suhongbo2017 <suhongbo2017@gmail.com>
2024-05-25 15:20:29 +0800

    第二个测试题完成

--- test2.py
+++ test2.py
@@ -1,8 +1,13 @@
-arr = [100000,600000,400000,200000,100000,0]
+arr = [1000000,600000,400000,200000,100000,0]
 rat = [0.01,0.015,0.03,0.05,0.075,0.1]
 bonus = 0
-# profit = input()
+profit = int(input())
 
 
 for idx in range(6):
-    print(idx)
+    if profit > arr[idx]:
+        bonus += (profit - arr[idx])* rat[idx]
+        print(bonus)
+        profit = arr[idx]
+        # break
+
